f6f07746273600727ff7a52f3876766633d2f3da
AishwaryaArun12 <jj4245888@gmail.com>
2022-07-11 10:26:47 +0530

    commited

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,27 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-const App = () => {
+const App = (slides) => {
+  const [index,setIndex]=useState(0);
+  function decrement(){
+    setIndex(index=>index-1)
+  }
+  function restart(){
+    setIndex(index=>0)
+  }
+  function increment(){
+    setIndex(index=>index+1)
+  }
   return (
-    <></>
+    <>
+    <div>
+      <h1>{slides[{index}].title}</h1>
+      <p>{slides[{index}].t}</p>
+    </div>
+    <button data-testid="button-prev" disabled={index===0} onClick={decrement}>Prev</button>
+    <button data-testid="button-restart" disabled={index===0} onClick={restart}>Restart</button>
+    <button data-testid="button-next" disabled={index===slides.length-1} onClick={increment}>Next</button>
+    </>
   )
 }
 
