49376cae06081d5f24df585ea90c668cca4f9237
AishwaryaArun12 <92584479+AishwaryaArun12@users.noreply.github.com>
2022-07-19 12:57:27 +0530

    commit 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,46 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
-const App = (slides) => {
-  const [index,setIndex]=useState(0);
-  function decrement(){
-    setIndex(index=>index-1)
+const App = (props) => {
+  const [index, setIndex] = useState(0);
+  function decrement() {
+    setIndex((index) => index - 1);
   }
-  function restart(){
-    setIndex(index=>0)
+  function restart() {
+    setIndex((index) => 0);
   }
-  function increment(){
-    setIndex(index=>index+1)
+  function increment() {
+    setIndex((index) => index + 1);
   }
   return (
     <>
-    <div>
-      <h1>{slides[{index}].title}</h1>
-      <p>{slides[{index}].t}</p>
-    </div>
-    <button data-testid="button-prev" disabled={index===0} onClick={decrement}>Prev</button>
-    <button data-testid="button-restart" disabled={index===0} onClick={restart}>Restart</button>
-    <button data-testid="button-next" disabled={index===slides.length-1} onClick={increment}>Next</button>
+      <div>
+        <h1>{props.slides[index].title}</h1>
+        <p>{props.slides[index].text}</p>
+      </div>
+      <button
+        data-testid="button-prev"
+        disabled={index === 0}
+        onClick={decrement}
+      >
+        Prev
+      </button>
+      <button
+        data-testid="button-restart"
+        disabled={index === 0}
+        onClick={restart}
+      >
+        Restart
+      </button>
+      <button
+        disabled={index === props.slides.length - 1}
+        data-testid="button-next"
+        onClick={increment}
+      >
+        Next
+      </button>
     </>
-  )
-}
-
+  );
+};
 
 export default App;
