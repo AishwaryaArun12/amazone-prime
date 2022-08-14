0b103ee1d0c6a18c3137ed54fe6fa52419cfaefa
AishwaryaArun12 <jj4245888@gmail.com>
2022-08-14 18:41:04 +0530

    commited

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { useState } from "react";
 import "../styles/App.css";
 
 const App = (props) => {
@@ -15,12 +15,12 @@ const App = (props) => {
   return (
     <>
     <div>
-      <h1 data-testid="title">{slides[{index}].title}</h1>
-      <p data-testid="text">{slides[{index}].t}</p>
+      <h1 data-testid="title">{props.slides[{index}].title}</h1>
+      <p data-testid="text">{props.slides[{index}].t}</p>
     </div>
     <button data-testid="button-prev" disabled={index===0} onClick={decrement}>Prev</button>
     <button data-testid="button-restart" disabled={index===0} onClick={restart}>Restart</button>
-    <button data-testid="button-next" disabled={index===slides.length-1} onClick={increment}>Next</button>      
+    <button data-testid="button-next" disabled={index===props.slides.length-1} onClick={increment}>Next</button>      
     </>
   );
 };
